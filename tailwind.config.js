b67326c592ece976cc9ecca8a6798b17907e9b1f
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-25 20:46:41 +0200

    Added basic styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gradientOne: "hsl(249, 99%, 64%)",
+        gradientTwo: "hsl(278, 94%, 30%)",
+        red: "hsl(0, 100%, 66%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGrayishViolet: "hsl(270, 3%, 87%)",
+        darkGrayishViolet: "hsl(279, 6%, 55%)",
+        veryDarkViolet: "hsl(278, 68%, 11%)",
+      },
+    },
   },
   plugins: [],
 };
