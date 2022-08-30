d111eedacdf1a25451e77873478cd8bcb2f8e13b
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-30 22:30:41 +0200

    Finding inputs.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// alert("Working");
-
+// elements
 cardNumbers = document.querySelectorAll(".card-number");
 cardNames = document.querySelectorAll(".card-name");
 cardDates = document.querySelectorAll(".card-date");
 cardCVCs = document.querySelectorAll(".card-cvc");
 
+// inputs
+fieldHolder = document.querySelector("#cardholder");
+fieldNumber = document.querySelector("#cardnumber");
+fieldMonth = document.querySelector("#cardmonth");
+fieldYear = document.querySelector("#cardyear");
+fieldCVC = document.querySelector("#cardcvc");
+
 for (const number of cardNumbers) {
   console.log(number.textContent);
 }
+
+const updateCardHolder = (name) => {};
+
+fieldHolder.addEventListener("change", (e) => console.log(e.target.value));
