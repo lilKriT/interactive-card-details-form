dd3f7f2e525a22f4ce41075ae02dcb8a15201442
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-30 22:18:41 +0200

    Finding elements.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Working");
+// alert("Working");
+
+cardNumbers = document.querySelectorAll(".card-number");
+cardNames = document.querySelectorAll(".card-name");
+cardDates = document.querySelectorAll(".card-date");
+cardCVCs = document.querySelectorAll(".card-cvc");
+
+for (const number of cardNumbers) {
+  console.log(number.textContent);
+}
